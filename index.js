469f2108d2e30d4be01d2ebfee0de69374219fa9
leobezimansky <71717961+leobezimansky@users.noreply.github.com>
2021-03-21 16:27:39 -0400

    sddfh

---
+++ index.js
@@ -0,0 +1,7 @@
+const quizContainer = document.getElementById("game");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("complete");
+//npm init -y
+//npm install -D parcel@next
+//"start": "parcel serve ./src/index.html",
+  //  "build": "parcel build ./src/index.html"
